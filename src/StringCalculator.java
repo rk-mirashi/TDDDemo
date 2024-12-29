badb20e3826cd8b8e6f77d7e5ac7104c65057f9c
rk-mirashi <mirashirushikesh@gmail.com>
2024-12-29 18:02:27 +0530

    added case to handle diferent delimiters

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,14 +1,26 @@
 public class StringCalculator {
 	int add(String input) {
-		String[] nl=input.split("\n");
+		
+		String[] nl=input.split("\n");//To handle new line in string
 		int sum=0;//to store sum of all numbers
+		String delimeter=nl[0].substring(nl[0].length()-1);//To handle different delimiters
+		
+		if (Character.isDigit(delimeter.charAt(0)))
+		{
+			delimeter=",";
+		}
+		
 		for(int l=0;l<nl.length;l++) {
-			
-			String[] s=nl[l].split(","); //converting string separated by commas to array 
+			 System.out.println(nl[l]); 
+			String[] s=nl[l].split(delimeter); //converting string separated by commas to array 
 			
 			for(int i = 0;i < s.length;i++) 
 			{
-			   sum += Integer.parseInt(s[i]);//adding integers after converting it to integer type
+				if (Character.isDigit(s[i].charAt(0)))
+				{
+					sum += Integer.parseInt(s[i]);//adding integers after converting it to integer type
+				}
+			   
 			}
 		}
 		return sum;
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -7,8 +7,9 @@ class StringCalculatorTest {
 	@Test
 	public void String_Calculator_Test() {
 		StringCalculator cal = new StringCalculator();
-		Assert.assertEquals(6, cal.add("1,2,3"), 0); //added case to handle comma separated numbers
-		Assert.assertEquals(6, cal.add("1\n2,3"), 0); //added case to handle new line and comma separated numbers
+		Assert.assertEquals(6, cal.add("1,2,3"), 0); //added case to check comma separated numbers
+		Assert.assertEquals(6, cal.add("1\n2,3"), 0); //added case to check new line and comma separated numbers
+		Assert.assertEquals(3, cal.add("//;\n1;2"), 0);//added case to check different delimiters
 	}
 
 }
